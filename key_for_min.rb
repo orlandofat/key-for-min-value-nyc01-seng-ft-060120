c924f68a7ff7002beca678df2dad09d250b58c61
Orlando Challenger <orlandofat@gmail.com>
2020-05-21 00:40:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,14 +3,14 @@ require 'pry'
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-   smallest_name = nil
+   smallest_key = nil
   smallest_value = 100
 
   name_hash.each do |key, value|
     if value <= smallest_value
       smallest_value = value
-      smallest_name = key
+      smallest_key = key
     end
   end
-  smallest_name
+  smallest_key
 end
