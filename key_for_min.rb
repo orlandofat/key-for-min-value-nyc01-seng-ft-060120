712a72da7b3a8ecc952c37259b457c8ce487e52d
Orlando Challenger <orlandofat@gmail.com>
2020-05-21 00:23:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,11 @@
+require 'pry'
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name_hash.each_with_object({}) do |k|
+   k.key(value.min)
+    binding.pry
+ 
+  end
 end
