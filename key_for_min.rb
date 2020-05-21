418311fbffd9c75082aee21c1f5f0625257d510b
Orlando Challenger <orlandofat@gmail.com>
2020-05-21 00:31:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,14 @@ require 'pry'
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.each_with_object({}) do |k, v|
-    puts "Key = #{k} / Value = #{nil}"
-    #binding.pry
- 
+   smallest_name = nil
+  smallest = 1000000
+
+  name_hash.each do |key, value|
+    if value <= smallest
+      smallest = value
+      smallest_name = key
+    end
   end
+  smallest_name
 end
